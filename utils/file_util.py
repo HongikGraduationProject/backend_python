aeaa7f2bfcd7de6677179f472fcfac4e09bfc7ad
qjvk2880 <qjvk2880@gmail.com>
2024-01-24 18:36:01 +0900

    Feat : 파일 삭제 기능 추가

--- utils/file_util.py
+++ utils/file_util.py
@@ -3,8 +3,16 @@ import os
 
 
 def convert_video_to_audio(filename):
-    if os.path.isfile(filename):
-        video = VideoFileClip(filename + ".mp4")
+    video_filename = filename + ".mp4"
+    if os.path.isfile(video_filename):
+        video = VideoFileClip(video_filename)
         new_filename = filename + ".mp3"
         video.audio.write_audiofile(new_filename)
+        video.close()
+        delete_file(video_filename)
         return new_filename
+
+
+def delete_file(filename):
+    if os.path.isfile(filename):
+        os.remove(filename)

ab0dfdfbffe9ba0c71aa2bb48b9635f88635f940
qjvk2880 <qjvk2880@gmail.com>
2024-01-24 18:47:40 +0900

    Feat : shortcode를 추출하는 함수 추가 및 print문 제거

--- utils/instagram_util.py
+++ utils/instagram_util.py
@@ -1,25 +1,35 @@
+from dto.shortform import ShortFormDownLoaded
+from utils import file_util
 import instaloader
 import os
-import file_util
+import re
+
+SHORTCODE_REGEX = r'(?:https?:\/\/)?(?:www\.)?instagram\.com\/?([a-zA-Z0-9\.\_\-]+)?\/([p]+)?([reel]+)?([tv]+)?([stories]+)?\/([a-zA-Z0-9\-\_\.]+)\/?([0-9]+)?'
 
 
-def download_reels_as_audio(video_url, uuid):
-    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'audios', uuid)
-
+def download_reels_as_audio(reels_url, uuid):
     L = instaloader.Instaloader(compress_json=False,
-                                dirname_pattern=filename,
                                 download_pictures=False,
                                 download_comments=False,
                                 download_video_thumbnails=False,
                                 download_geotags=False)
-    post = instaloader.Post.from_shortcode(L.context, "C2XAeNXxv36")
-    # print(video_url)
-    # print(post.date_utc)
-    # # print(post.title)
-    # print(post.caption)
-    # print(post.location)
-    # print(filename)
+    post = instaloader.Post.from_shortcode(L.context, extract_shortcode(reels_url))
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'audios', uuid)
     L.download_pic(url=post.video_url, filename=filename, mtime=post.date_local)
     new_filename = file_util.convert_video_to_audio(filename)
+    return ShortFormDownLoaded(
+        uuid=uuid,
+        description=post.caption,
+        file_name=new_filename,
+        url=reels_url
+    )
 
 
+def extract_shortcode(reels_url):
+    match = re.search(SHORTCODE_REGEX, reels_url)
+
+    if match:
+        shortcode = match.group(6)
+        return shortcode
+
+    return None
